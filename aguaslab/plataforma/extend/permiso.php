8f75cf4bdcd7dc0ad812eca7c5b9c01b30c88a5a
Andrés Corral <33558356+AndresCorral@users.noreply.github.com>
2019-06-27 10:25:56 -0500

    Cambio nombres de administrador y cliente
    Se cambian los nombres de ADMINISTRADOR por SUPERUSUARIO y CLIENTE por ADMINISTRACION

--- aguaslab/plataforma/extend/permiso.php
+++ aguaslab/plataforma/extend/permiso.php
@@ -1,5 +1,5 @@
 <?php 
-if ($_SESSION['nivel'] != 'ADMINISTRADOR' && $_SESSION['nivel'] != 'CLIENTE' && $_SESSION['nivel'] != 'PISCINERO') {
+if ($_SESSION['nivel'] != 'SUPERUSUARIO' && $_SESSION['nivel'] != 'ADMINISTRACION' && $_SESSION['nivel'] != 'PISCINERO') {
 	header('location:bloqueo.php');
 }
 ?>
